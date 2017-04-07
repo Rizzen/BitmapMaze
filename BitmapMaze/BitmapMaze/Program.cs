c6b9db6aaee3dd58c8e716d7c72a7a2ff903c6b6
Rizzen <Rizzen@DESKTOP-NDFFLI4>
2017-04-08 00:57:24 +0300

    Added Classes

--- BitmapMaze/BitmapMaze/Program.cs
+++ BitmapMaze/BitmapMaze/Program.cs
@@ -14,15 +14,15 @@ namespace BitmapMaze
 
         static void Main(string[] args)
         {
-            const int CELL_SIZE = 20;
-            const int MAZE_X = 10;
-            const int MAZE_Y = 7;
+            const int CELL_SIZE = 15;
+            const int MAZE_X = 1000;
+            const int MAZE_Y = 1000;
 
-            var cells = new int[MAZE_X, MAZE_Y];
+            //var cells = new int[MAZE_X, MAZE_Y];
             var bmp = new Bitmap(MAZE_X * CELL_SIZE + 1, MAZE_Y * CELL_SIZE + 1);
-
             var g = Graphics.FromImage(bmp);
             var p = new Pen(Color.Black);
+
             for (int i = 0; i < MAZE_X; i++)
             {
                 for (int j = 0; j < MAZE_Y; j++)
@@ -32,8 +32,18 @@ namespace BitmapMaze
                 }
             }
 
-            bmp.Save($"bmp{DateTimeOffset.Now.ToUnixTimeSeconds()}.bmp");
+            for (int i = 0; i < bmp.Size.Height; i++)
+            {
+                for (int j = 0; j < bmp.Size.Width; j++)
+                {
+                    bmp.SetPixel(i,j, Color.Red);
+                    Console.WriteLine($" {i} {j} {bmp.GetPixel(i, j)} ");
+                }
+            }
 
+           bmp.Save($"bmp{DateTimeOffset.Now.ToUnixTimeSeconds()}.bmp");
+           
+            Console.WriteLine(Image.GetPixelFormatSize(bmp.PixelFormat));
             Console.ReadLine();
         }
     }
