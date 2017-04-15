19989307c6a9fced2f7d308fe91616e9e07d88ae
Rizzen <onemangd@gmail.com>
2017-04-15 21:55:04 +0300

    fix

--- BitmapMaze/BitmapMaze/Cell.cs
+++ BitmapMaze/BitmapMaze/Cell.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BitmapMaze.Handlers;
-using BitmapMaze.Args;
 using System.Drawing;
 using System.Drawing.Imaging;
 
